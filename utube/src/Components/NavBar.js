4f2f1da310ceaa4ac179bf535bc1a1428c56321d
Dylan Feres <dferes23@gmail.com>
2021-06-10 00:58:24 -0700

    updated the NavbarToggler

--- utube/src/Components/NavBar.js
+++ utube/src/Components/NavBar.js
@@ -1,14 +1,21 @@
 import React, { useContext, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, NavItem, NavbarBrand, NavbarToggler, Collapse, Button } from 'reactstrap';
-// import Sidebar from 'react-bootstrap-sidebar';
 import './NavBar.css';
 import UserContext from '../FormContext';
 import useInputFilter from '../hooks/useInputFilter';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUser,
+  faBook,
+  faHistory,
+} from "@fortawesome/free-solid-svg-icons";
 
-const  UTubeLogo = process.env.PUBLIC_URL + 'images/UTube_Button2.png';
+const UTubeLogo = process.env.PUBLIC_URL + 'images/UTube_Button2.png';
 const defaultAvatarImage = process.env.PUBLIC_URL + 'images/default_avatar_icon.png';
 const searchButtonIcon = process.env.PUBLIC_URL + 'images/search_glass_symbol.png';
+const sidebarMenuIcon = process.env.PUBLIC_URL + 'images/hamburger_icon5.png';
 
 
 const NavBar = ({ isLoggedIn, setIsLoggedIn }) => {
@@ -58,23 +65,50 @@ const NavBar = ({ isLoggedIn, setIsLoggedIn }) => {
           <Button outline color='primary' className='navbar-sign-in-button'>SIGN IN
           <img className='default-avatar-icon' src={defaultAvatarImage} alt=''></img>
           </Button> 
-        }    
+        }
+        <div className='collapse-menu-div'>
         <Collapse className='collapseMenu' isOpen={!collapsed} navbar>
+          
+          <div className='collapse-menu-brand-div'>
+            <button onClick={toggleNavbar} className='collapse-menu-hamburger-button'>
+              <img className='collapse-menu-hamburger-icon' src={sidebarMenuIcon} alt=''/>
+            </button>
+            <NavbarBrand href='/' className='UTube-icon-side-bar'>
+              <img className='UTubeLogo-sidebar' src={UTubeLogo} alt='UTube'/>UTube
+            </NavbarBrand>
+          </div>
+          
+          <hr/>
           <Nav navbar> 
             <NavItem>
-              <NavLink to='/'>Home</NavLink>  
+              <FontAwesomeIcon icon={faHome} className="font-awesome-menu-icon" /> 
+              <NavLink to='/'>Home</NavLink> 
             </NavItem>
             <NavItem>
-              <NavLink to='/'>Subscriptions</NavLink>  
+            <FontAwesomeIcon icon={faUser} className="font-awesome-menu-icon" /> 
+              <NavLink to='/subscriptions'>Subscriptions</NavLink>  
+            </NavItem>
+            <hr/>
+            <NavItem>
+            <FontAwesomeIcon icon={faBook} className="font-awesome-menu-icon" /> 
+              <NavLink to='library'>Library</NavLink>  
             </NavItem>
             <NavItem>
-              <NavLink to='/'>Library</NavLink>  
+            <FontAwesomeIcon icon={faHistory} className="font-awesome-menu-icon" /> 
+              <NavLink to='/history'>History</NavLink>  
             </NavItem>
-            <NavItem>
-              <NavLink to='/'>History</NavLink>  
-            </NavItem>
+            <hr/>
+            { !isLoggedIn &&
+            <div className='collapse-menue-sign-in-button-div'>
+              <p>Sign in to like videos, comment, and subscribe.</p>
+              <Button outline color='primary' className='sidebar-sign-in-button'>SIGN IN
+                <img className='default-avatar-icon' src={defaultAvatarImage} alt=''></img>
+              </Button> 
+            </div>
+            }
           </Nav>
         </Collapse>
+        </div> 
       </Navbar>
     </div>
   );
