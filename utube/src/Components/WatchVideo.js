5b255faf8da6333e459bc46837836d35cabc02d1
Dylan Feres <dferes23@gmail.com>
2021-06-12 12:05:53 -0700

    Layout for video viweing, including video details, suggested videos sidebard and comments

--- utube/src/Components/WatchVideo.js
+++ utube/src/Components/WatchVideo.js
@@ -1,23 +1,33 @@
 import { useContext } from 'react';
 import UserContext from '../FormContext';
+import './WatchVideo.css';
 
 const WatchVideo = () => {
   const { currentVideo } = useContext(UserContext);
 
   return (
-    <div>
-      <h1>Meh: {Object.keys(currentVideo).length}</h1>
-      <p>{currentVideo.id}</p>
-      <p>{currentVideo.createdAt}</p>
-      <p>{currentVideo.title}</p>
-      <p>{currentVideo.description}</p>
-      <p>{currentVideo.url}</p>
-      <p>{currentVideo.username}</p>
-      <p>{currentVideo.thumbnailImage}</p>
-      <p>{currentVideo.views.length}</p>
-      <p>{currentVideo.likes.length}</p>
-      <p>{currentVideo.comments.length}</p>
-    </div>  
+    <div className='video-watch-main-div'>
+      <div className='video-watch-left-div-container'>
+        <div className='video-player-div'>
+          {/* <p>{currentVideo.id}</p> */}
+          {/* <p className='video-player-description'>{currentVideo.description}</p> */}
+          {/* <p className='video-player-video-url'>{currentVideo.url}</p> */}
+          {/* <p className='video-player-username'>{currentVideo.username}</p> */}
+          {/* <p>{currentVideo.thumbnailImage}</p> */}
+          {/* <p>{currentVideo.likes.length}</p>
+          <p>{currentVideo.comments.length}</p> */}
+        </div>
+        <div className='video-player-video-details'>
+          <p className='video-player-title'>{currentVideo.title}</p>
+          <p className='video-player-views'>{currentVideo.views.length} views .{currentVideo.createdAt}</p>
+        </div>
+        <hr className='video-watch-bottom-hr'/>
+      </div>
+      
+      <div className='video-watch-suggested-videos-div'>
+
+      </div>
+    </div>
   );  
 }
 
