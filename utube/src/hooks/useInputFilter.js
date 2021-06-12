3c490965957977a59454f257b7fe6b908fa9c119
Dylan Feres <dferes23@gmail.com>
2021-06-11 21:09:53 -0700

    Modified to accept an update function from a useState

--- utube/src/hooks/useInputFilter.js
+++ utube/src/hooks/useInputFilter.js
@@ -2,29 +2,26 @@ import { useState } from 'react';
 import UTubeApi from '../api';
 
 
-const useInputFilter = ({ defaultList, apiMethod, termKey }) => {
+const useInputFilter = ({ defaultList=[], apiMethod, termKey, globalUpdateFunction }) => {
   const [ filter, setFilter ] = useState({ [termKey]: ''});
-  const [ resultList, setResultList ] = useState(defaultList);
-  const [ isEmpty, setIsEmpty ] = useState(false);
     
   const handleChange = evt => {
-    const { name, value } = evt.target;  
+    const { name, value } = evt.target;
     setFilter( data => ({...data, [name]: value}));
   };
     
   const handleSubmit = async evt => {
-    evt.preventDefault();  
+    evt.preventDefault();
     const filterTerm = Object.keys(filter);
 
-    const res = filter[filterTerm[0]] === '' ?
-      defaultList
-      : await UTubeApi[ [apiMethod] ](filter);
-
-    setResultList(res);
-    setIsEmpty( res.length === 0 ? true: false );
+    globalUpdateFunction(
+      filter[filterTerm[0]] === '' ?
+        defaultList
+        : await UTubeApi[ [apiMethod] ](filter)
+    );
   };
 
-  return [ resultList, filter, handleChange, handleSubmit, isEmpty ];
+  return [ filter, handleChange, handleSubmit ];
 };
 
 export default useInputFilter;
