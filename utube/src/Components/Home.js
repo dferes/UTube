9da7bae245f1dc3f2bd08cc92085e10af21448f0
Dylan Feres <dferes23@gmail.com>
2021-06-12 02:03:06 -0700

    Basic grid of videoGridCards

--- utube/src/Components/Home.js
+++ utube/src/Components/Home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useContext } from 'react';
+import UserContext from '../FormContext';
+import VideoGridCard from './VideoGridCard';
+import './Home.css';
+
 
 const Home = () => {
+  // const { allVideoList, currentVideo, setCurrentVideo } = useContext(UserContext);
+  const { allVideoList } = useContext(UserContext);
+
   return (
-    <>
-    </>  
+    <div className='home-video-grid-div'>
+      <hr className='home-video-grid-hr' />
+      { allVideoList.map( video => (
+        <VideoGridCard 
+          key={video.id}
+          id={video.id}
+          createdAt={video.createdAt}
+          title={video.title}
+          username={video.username}
+          url={video.url}
+          description={video.description}
+          thumbnail={video.thumbnailImage}
+        />
+      ))}
+    </div>  
   );
 };
 
