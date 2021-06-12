42b96a97c11b57173d43614cf4e1bd4f13d663a3
Dylan Feres <dferes23@gmail.com>
2021-06-11 21:14:43 -0700

    videoSearchList is now commited to local storage

--- utube/src/Components/App.js
+++ utube/src/Components/App.js
@@ -1,89 +1,97 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
-import UTubeApi from '../api';
+// import UTubeApi from '../api';
 import NavBar from './NavBar';
 import FormContext from '../FormContext';
 import useLocalStorage from '../hooks/useLocalStorage';
+// import useInputFilter from '../hooks/useInputFilter';
 import './App.css';
 
 
 function App() {
-  const [ loginFormData, setLoginFormData ] = useState({});
-  const [ signupFormData, setSignupFormData ] = useState( {} );
-  const [ userFormData, setUserFormData ] = useState({});
+  // const [ loginFormData, setLoginFormData ] = useState({});
+  // const [ signupFormData, setSignupFormData ] = useState( {} );
+  // const [ userFormData, setUserFormData ] = useState({});
 
-  const [ userToken, setUserToken ] = useLocalStorage('userToken', '');
+  // const [ userToken, setUserToken ] = useLocalStorage('userToken', '');
   const [ user, setUser ] = useLocalStorage('user',  {} );
   const [ isLoggedIn, setIsLoggedIn ] = useLocalStorage('isLoggedIn', false);
+  const [ videoSearchList, setVideoSearchList ] = useLocalStorage('videoSearchList', []);
+  // const [ errorMessage, setErrorMessage ] = useState({});
+  // const [ showSuccessMessage, setShowSuccessMessage ] = useState(false);
 
-  const [ errorMessage, setErrorMessage ] = useState({});
-  const [ showSuccessMessage, setShowSuccessMessage ] = useState(false);
 
   /** Logs the user out if isLoggedIn changes to false */
-  useEffect( () => {
-    const logout = () => {
-      setUserToken('');
-      setUser({});
-      resetUserFormData();
-    };
+  // useEffect( () => {
+  //   const logout = () => {
+  //     setUserToken('');
+  //     setUser({});
+  //     resetUserFormData();
+  //   };
   
-    if (!isLoggedIn) logout();
-  }, [isLoggedIn, setUserToken, setUser]);
+  //   if (!isLoggedIn) logout();
+  // }, [isLoggedIn, setUserToken, setUser]);
 
 
-  const resetUserFormData = () => {
-    setLoginFormData({});
-    setSignupFormData({});
-    setUserFormData({});
-  };
+  // const resetUserFormData = () => {
+  //   setLoginFormData({});
+  //   setSignupFormData({});
+  //   setUserFormData({});
+  // };
   
-  const handleUserFormChange = (evt, login=false, signup=false) => {
-    const { name, value } = evt.target;
-    if (login) setLoginFormData( data => ({...data, [name]: value}) );
-    else if(signup)setSignupFormData( data => ({...data, [name]: value}) );
-    else setUserFormData( data => ({...data, [name]: value}) );
-  };
+  // const handleUserFormChange = (evt, login=false, signup=false) => {
+  //   const { name, value } = evt.target;
+  //   if (login) setLoginFormData( data => ({...data, [name]: value}) );
+  //   else if(signup)setSignupFormData( data => ({...data, [name]: value}) );
+  //   else setUserFormData( data => ({...data, [name]: value}) );
+  // };
 
-  const handleUserFormSubmit = async (evt, apiMethod, formInfo) => {
-    evt.preventDefault();
-    try {
-      if(['logIn', 'signup'].includes(apiMethod)){ // can be logIn, signup or update
-        const token = await UTubeApi[ [apiMethod] ](formInfo);
-        setUserToken(token);
-      }else {
-        UTubeApi.setToken(userToken);
-        await UTubeApi[ [apiMethod] ](formInfo);
-      }
-      const user_ = await UTubeApi.getUser(formInfo.username);
-      setUser(user_);
-      resetUserFormData();
-      setErrorMessage({});
-      setShowSuccessMessage(apiMethod==='update'? true: false);
-      setIsLoggedIn(true);
-    }catch(err) { setErrorMessage({ [apiMethod]: err }); }
-  };
+  // const handleUserFormSubmit = async (evt, apiMethod, formInfo) => {
+  //   evt.preventDefault();
+  //   try {
+  //     if(['logIn', 'signup'].includes(apiMethod)){ // can be logIn, signup or update
+  //       const token = await UTubeApi[ [apiMethod] ](formInfo);
+  //       setUserToken(token);
+  //     }else {
+  //       UTubeApi.setToken(userToken);
+  //       await UTubeApi[ [apiMethod] ](formInfo);
+  //     }
+  //     const user_ = await UTubeApi.getUser(formInfo.username);
+  //     setUser(user_);
+  //     resetUserFormData();
+  //     setErrorMessage({});
+  //     setShowSuccessMessage(apiMethod==='update'? true: false);
+  //     setIsLoggedIn(true);
+  //   }catch(err) { setErrorMessage({ [apiMethod]: err }); }
+  // };
 
   const formFunctions = {
-    user: user
+    user: user,
+    setUser: setUser,
+    videoSearchList: videoSearchList,
+    setVideoSearchList: setVideoSearchList,
+    isLoggedIn: isLoggedIn,
+    setIsLoggedIn: setIsLoggedIn
     // handleFormChange: handleFormChange,
     // handleFormSubmit: handleFormSubmit,
     // loginFormData: loginFormData,
     // signupFormData: signupFormData,
     // userFormData: userFormData
   };
+  
+
+
 
   return (
     <div className="App">
       <FormContext.Provider value={formFunctions}>
         <BrowserRouter>
-          <NavBar 
-            isLoggedIn={isLoggedIn} 
-            setIsLoggedIn={setIsLoggedIn}
-          />
+          <NavBar />
           <main>
             <Routes 
-              userToken={userToken}
+              // userToken={userToken}
               // errorMessage={errorMessage}
               // showSuccessMessage={showSuccessMessage}
               // setShowSuccessMessage={setShowSuccessMessage}
