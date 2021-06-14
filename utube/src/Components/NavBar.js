e0f546074666ec71976db7a61385d3da5ad996c7
Dylan Feres <dferes23@gmail.com>
2021-06-14 13:44:16 -0700

    remade navbar collapse menu with UncontrolledDropdownMenu

--- utube/src/Components/NavBar.js
+++ utube/src/Components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavbarBrand, NavbarToggler, Collapse, Button } from 'reactstrap';
+import { Navbar, NavbarBrand, Button, UncontrolledDropdown, 
+  DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import useInputFilter from '../hooks/useInputFilter';
 import './NavBar.css';
 import UserContext from '../FormContext';
@@ -21,7 +22,6 @@ const sidebarMenuIcon = process.env.PUBLIC_URL + 'images/hamburger_icon5.png';
 const NavBar = () => {
   // const { user, videoSearchList, setVideoSearchList, isLoggedIn } = useContext(UserContext);
   const { videoSearchList, setVideoSearchList, isLoggedIn } = useContext(UserContext);
-  const [collapsed, setCollapsed] = useState(true);
   const [ formSubmit, setFormSubmit ] = useState(false);
   const history = useHistory();
 
@@ -31,7 +31,6 @@ const NavBar = () => {
     handleSubmit
   ] = useInputFilter({ apiMethod: 'videoSearch', termKey: 'title', globalUpdateFunction: setVideoSearchList} );
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
   
   useEffect( () => {
     const goToResultsPage = async () => {
@@ -44,103 +43,108 @@ const NavBar = () => {
   }, [videoSearchList, formSubmit, setFormSubmit, history]);
 
   return (
-    <div>
-      <Navbar fixed='top' className='UTube-navbar navbar-dark'>
-        <div className='navbarBrand'>
-          <NavbarToggler onClick={toggleNavbar} className='menuButton'/>
-          <NavbarBrand href='/' className='ml-auto'>
-            <img className='UTubeLogo' src={UTubeLogo} alt='UTube'/>UTube
-          </NavbarBrand>
-        </div>
-
-        <div className='video-search-div'>
-          <form className='video-title-search-form' onSubmit={ async (evt) => {
-            await handleSubmit(evt);
-            await setFormSubmit(true);
-          } }>
-            <label htmlFor='video-title-search' />
-            <input 
-              className='video-title-search-input'
-              id='video-title-search'
-              name='title'
-              placeholder='Search'
-              value={filter.title}
-              onChange={handleChange}
-            />
-            <button className='video-title-search-button'>
-              <img 
-                className='video-title-search-button-icon' 
-                src={searchButtonIcon} 
-                alt=''/>
-            </button>    
-          </form>
-        </div>
-        { !isLoggedIn &&
-          <Button outline color='primary' className='navbar-sign-in-button'>SIGN IN
-          <img className='default-avatar-icon' src={defaultAvatarImage} alt='' />
-          </Button> 
-        }
-        <div className='collapse-menu-div'>
-        <Collapse className='collapseMenu' isOpen={!collapsed} navbar>
-          {/* <div className='collapse-menu-div'> */}
-            <div className='collapse-menu-brand-div'>
-              <button onClick={toggleNavbar} className='collapse-menu-hamburger-button'>
-                <img className='collapse-menu-hamburger-icon' src={sidebarMenuIcon} alt=''/>
-              </button>
-              <NavbarBrand href='/' className='UTube-icon-side-bar'>
-                <img className='UTubeLogo-sidebar' src={UTubeLogo} alt='UTube'/>UTube
-              </NavbarBrand>
-            </div>
-            
-            <Nav navbar> 
-              <NavItem>
-                <div className='nav-link-div'>
-                  <FontAwesomeIcon icon={faHome} className="font-awesome-menu-icon" /> 
-                  <NavLink to='/'>
-                    <p className='navlink-text'>Home</p>
-                  </NavLink> 
-                </div>
-              </NavItem>
-              <NavItem>
-                <div className='nav-link-div'>
-                  <FontAwesomeIcon icon={faUser} className="font-awesome-menu-icon" /> 
-                    <NavLink to='/subscriptions'>
-                      <p className='navlink-text'>Subscriptions</p>
-                    </NavLink>  
-                </div>
-              </NavItem>
-              <hr/>
-              <NavItem>
-                <div className='nav-link-div'>
-                  <FontAwesomeIcon icon={faBook} className="font-awesome-menu-icon" /> 
-                    <NavLink to='library'>
-                      <p className='navlink-text'>Library</p>
-                    </NavLink>  
-                </div>
-              </NavItem>
-              <NavItem>
-                <div className='nav-link-div'>
-                  <FontAwesomeIcon icon={faHistory} className="font-awesome-menu-icon" /> 
-                    <NavLink to='/history'>
-                    <p className='navlink-text'>History</p>
-                    </NavLink>  
-                </div>
-              </NavItem>
-              <hr/>
-              { !isLoggedIn &&
-              <div className='collapse-menue-sign-in-button-div'>
-                <p>Sign in to like videos, comment, and subscribe.</p>
-                <Button outline color='primary' className='sidebar-sign-in-button'>SIGN IN
-                  <img className='default-avatar-icon' src={defaultAvatarImage} alt=''></img>
-                </Button> 
+    <Navbar fixed='top' className='UTube-navbar navbar-dark'>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle 
+          className='navbar-dropdown-menu-toggle-button' nav
+          style={{
+            backgroundColor: 'rgb(32, 32, 32)', 
+            width: '4em',
+            marginTop: '-0.8em',
+            height: '3em',
+            marginLeft: '0.9em'
+          }}  
+        />
+        <img className='collapse-menu-hamburger-icon-collapse' src={sidebarMenuIcon} alt=''/>
+        <DropdownMenu className='navbar-dropdown-menu' left
+          style={{
+            backgroundColor: 'rgb(32, 32, 32)',
+            marginLeft: '0',
+            borderWidth: '0'
+          }}
+        >
+          <DropdownItem>
+            <NavLink to='/'>
+              <div className='nav-link-div'>
+                <FontAwesomeIcon icon={faHome} className="font-awesome-menu-icon" /> 
+                  <p className='navlink-text'>Home</p> 
               </div>
-              }
-            </Nav>
-          {/* </div> */}
-        </Collapse>
-        </div>
-      </Navbar>
-    </div>
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem>
+            <NavLink to='/subscriptions'>
+              <div className='nav-link-div'>
+                <FontAwesomeIcon icon={faUser} className="font-awesome-menu-icon" /> 
+                  <p className='navlink-text'>Subscriptions</p>
+              </div>
+            </NavLink>  
+          </DropdownItem>
+          <DropdownItem divider style={{backgroundColor: 'rgb(64, 64, 64)'}}/>
+          <DropdownItem>
+            <NavLink to='library'>
+              <div className='nav-link-div'>
+                <FontAwesomeIcon icon={faBook} className="font-awesome-menu-icon" /> 
+                  <p className='navlink-text'>Library</p>
+              </div>
+            </NavLink>  
+          </DropdownItem>
+          <DropdownItem>
+            <NavLink to='/history'>
+              <div className='nav-link-div'>
+                <FontAwesomeIcon icon={faHistory} className="font-awesome-menu-icon" /> 
+                <p className='navlink-text'>History</p>      
+              </div>
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem divider style={{backgroundColor: 'rgb(64, 64, 64)'}} />
+
+          { !isLoggedIn &&
+            <div className='collapse-menu-sign-in-button-div'>
+              <p>Sign in to like videos, comment, and subscribe.</p>
+              <Button outline color='primary' className='sidebar-sign-in-button'>SIGN IN
+                <img className='default-avatar-icon' src={defaultAvatarImage} alt=''></img>
+              </Button> 
+            </div>
+          }
+        </DropdownMenu>
+      </UncontrolledDropdown>
+      <div style={{
+          marginLeft: '-14em',
+          marginBottom: '0.6em'
+      }}>
+        <NavbarBrand href='/' >
+          <img className='UTubeLogo' src={UTubeLogo} alt='UTube'/>UTube
+        </NavbarBrand>
+      </div>    
+
+      <div className='video-search-div'>
+        <form className='video-title-search-form' onSubmit={ async (evt) => {
+          await handleSubmit(evt);
+          await setFormSubmit(true);
+        } }>
+          <label htmlFor='video-title-search' />
+          <input 
+            className='video-title-search-input'
+            id='video-title-search'
+            name='title'
+            placeholder='Search'
+            value={filter.title}
+            onChange={handleChange}
+          />
+          <button className='video-title-search-button'>
+            <img 
+              className='video-title-search-button-icon' 
+              src={searchButtonIcon} 
+              alt=''/>
+          </button>    
+        </form>
+      </div>
+      { !isLoggedIn &&
+        <Button outline color='primary' className='navbar-sign-in-button'>SIGN IN
+        <img className='default-avatar-icon' src={defaultAvatarImage} alt='' />
+        </Button> 
+      }
+    </Navbar>
   );
 };
 
