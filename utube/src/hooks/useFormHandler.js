dcaca47d956d0c7e81fd479c72f7f5d85adf66fa
Dylan Feres <dferes23@gmail.com>
2021-06-18 19:22:50 -0700

    added try catch block

--- utube/src/hooks/useFormHandler.js
+++ utube/src/hooks/useFormHandler.js
@@ -2,29 +2,39 @@ import { useState } from 'react';
 import UTubeApi from '../api';
 
 
-const useFormHandler = ({ apiMethod, globalUpdateFunction, starterData={} }) => {
+const useFormHandler = ({ apiMethod, globalUpdateFunction, starterData={}, successMessage_='' }) => {
   const [ data, setData ] = useState( {...starterData} );
+  const [ errorMessage, setErrorMessage] = useState(false);
+  const [ successMessage, setSuccessMessage] = useState(false);
     
   const handleChange = evt => {
     const { name, value } = evt.target;
     setData( data => ({...data, [name]: value}));
   };
+
     
   const handleSubmit = async (evt, extraData=null) => {
     evt.preventDefault();
-    let objectParameter = data;
-    if(extraData) objectParameter = { ...objectParameter, ...extraData};
-    
-    let res = await UTubeApi[ [apiMethod] ](objectParameter);
-    if(data.password) delete data.password;
-    res = {...res, ...data};           // this may be unnecessary, double check
+    try {
+      let objectParameter = data;
 
-    await globalUpdateFunction( res );
-    setData({});
-    evt.target.reset();
+      if(extraData) objectParameter = { ...objectParameter, ...extraData};
+      let res = await UTubeApi[ [apiMethod] ](objectParameter);
+      res = {...res, ...data};           // this may be unnecessary, double check
+      
+      if(data.password) delete data.password;
+      await globalUpdateFunction( res );
+      setData({});
+      evt.target.reset();
+      if (successMessage_) setSuccessMessage(successMessage_);
+    } 
+    catch(err) {
+      setErrorMessage(err);
+    } 
+
   };
 
-  return [ data, handleChange, handleSubmit ];
+  return [ data, handleChange, handleSubmit, errorMessage, successMessage, setSuccessMessage ];
 };
 
 export default useFormHandler;
