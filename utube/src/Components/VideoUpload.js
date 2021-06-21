dda3263d36105edfe72c7c529a7a0248e9ec07f3
Dylan Feres <dferes23@gmail.com>
2021-06-21 13:10:35 -0700

    Removed some comments, secure_url is now obtained from the external api post request and now automatically adds thumbnail to db when video form is submitted

--- utube/src/Components/VideoUpload.js
+++ utube/src/Components/VideoUpload.js
@@ -4,18 +4,18 @@ import UserContext from '../FormContext';
 import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import useFormHandler from '../hooks/useFormHandler';
 import './VideoUpload.css'
+import LoadingSpinner from './LoadingSpinner';
 
 const VideoUpload = () => {
-  const [ url, setUrl] = useState('');      // --------------setUrl when video has been uploaded to cloud
+  const [ url, setUrl] = useState('');
   const [ showSubmitButton, setShowSubmitButton] = useState(false);
+  const [ showLoadingSpinner, setShowLoadingSpinner] = useState(false);
   const [ videoUploaded, setVideoUploaded ] = useState(false);
+  
+  const [ thumbnailUrl, setThumbnailUrl ] = useState(false); 
   const { user } = useContext(UserContext);
   const history = useHistory();
-  // cloud name:                 dilw67t91
-  // unsigned upload preset:     mszj6nzh
-  // API Key:                    956736391961359
-  // API SecretKey:              jKNTP4fx5B3E3bfUhGuXRuqEJLc
-  // API Environment Variable:   CLOUDINARY_URL=cloudinary://956736391961359.jKNTP4fx5B3E3bfUhGuXRuqEJLc@dilw67t91 
+  
 
   const [ data, handleChange, handleSubmit, errorMessage ] = useFormHandler({ 
     apiMethod: 'makeVideo', 
@@ -26,50 +26,45 @@ const VideoUpload = () => {
 
 
   const uploadFile = async (file) => {
-    console.log('The file Data: ', file[0]);
     const url = "https://api.cloudinary.com/v1_1/dilw67t91/upload";
-
     const formData = new FormData();
 
     formData.append("file", file[0]);
     formData.append("upload_preset", "mszj6nzh");
-    console.log('formData: ', formData);
 
     const res = await fetch(url, {
       method: 'POST',
       body: formData
     });
-    console.log('res---------->',res.url);
-    setUrl(res.url);
+
+    console.log('The response: ', res);
+    const resJson = await res.json();
+    setUrl(resJson.secure_url);
+    const thumbUrl = resJson.secure_url.substring(0, resJson.secure_url.length - 3) + 'jpg';
+    setThumbnailUrl(thumbUrl);
   }
 
 
-  // const handleVideoUpload = async () => {
-  //   if (fileSelect) {
-  //     console.log('file selected: ', fileSelect);
-  //     fileSelect.current.click();
-  //   }
-  // }
-
   const handleFile = (file) => {
-    console.log('the file is: ', file);
+    setShowLoadingSpinner(true);
     uploadFile(file);
   }
 
   useEffect( () => {
-    if(url){
+    if(thumbnailUrl) {
+      setShowLoadingSpinner(false);
       setShowSubmitButton(true);
-      console.log('------------>>>>>>useEffect');
-    } 
-  }, [url]);
+    }
+
+  }, [thumbnailUrl, setShowLoadingSpinner, setShowSubmitButton]);
 
 
   useEffect( () => {
     if(videoUploaded){
-      console.log('------------>>>>>>useEffect2');
       history.push(`/profile/${user.username}`);
     } 
   }, [videoUploaded, user, history]);
+
 
 
   return (
@@ -78,27 +73,24 @@ const VideoUpload = () => {
         className='upload-form'
         onSubmit={ async (evt) => await handleSubmit(evt, {
           username: user.username,
-          url: url
+          url: url,
+          thumbnailImage: thumbnailUrl
         })} 
       >
         <FormGroup>
-            <Label className='upload-form-label' for='url'>File</Label>
-            {/* <Button color='primary'
-              className="upload-form-browse-button"
-              onClick={handleVideoUpload}
-              type="button"
-            >
-              Browse
-            </Button> */}
-            <Input 
-              ref={fileSelect}
-              type='file'
-              accept='video/*'
-              // style={{ display: 'none' }}
-              name='url'
-              id='file'            
-              onChange={(evt) => handleFile(evt.target.files)} // target.files or target.file ?????
+          <Label className='upload-form-label' for='url'>File</Label>
+          <Input 
+            ref={fileSelect}
+            type='file'
+            accept='video/*'
+            style={{ width: '20em', float: 'left', marginLeft: '1.5em' }}
+            name='url'
+            id='file'            
+            onChange={(evt) => handleFile(evt.target.files)}
           />
+          <div style={{float: 'right', width:'3em', height: '2.8em'}}>
+            { showLoadingSpinner  && <LoadingSpinner /> }
+          </div>
         </FormGroup>
         <FormGroup>
           <Label className='upload-form-label' for='title'>Title</Label>
@@ -113,7 +105,7 @@ const VideoUpload = () => {
             />
         </FormGroup>
         <FormGroup>
-          <Label className='upload-form-label' for='description'>About</Label>
+          <Label className='upload-form-label' for='description'>Description</Label>
           <Input type='textarea'
             name='description'
             id='about'
