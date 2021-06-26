787c1c54afc2d9734b87ce8951db2924a2b78612
Dylan Feres <dferes23@gmail.com>
2021-06-25 22:12:42 -0700

    replaced default value of useState messages to empty string instead of boolean

--- utube/src/hooks/useFormHandler.js
+++ utube/src/hooks/useFormHandler.js
@@ -4,8 +4,8 @@ import UTubeApi from '../api';
 
 const useFormHandler = ({ apiMethod, globalUpdateFunction, successMessage_='' }) => {
   const [ data, setData ] = useState( {} );
-  const [ errorMessage, setErrorMessage] = useState(false);
-  const [ successMessage, setSuccessMessage] = useState(false);
+  const [ errorMessage, setErrorMessage] = useState('');
+  const [ successMessage, setSuccessMessage] = useState('');
     
   const handleChange = evt => {
     const { name, value } = evt.target;
@@ -33,7 +33,8 @@ const useFormHandler = ({ apiMethod, globalUpdateFunction, successMessage_='' })
 
   };
 
-  return [ data, handleChange, handleSubmit, errorMessage, successMessage, setSuccessMessage ];
+  return [ data, handleChange, handleSubmit, errorMessage, setErrorMessage, successMessage, setSuccessMessage ];
 };
+
 
 export default useFormHandler;
